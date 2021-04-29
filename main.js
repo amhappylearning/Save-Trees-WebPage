2835fd2bd3e117cea63044b2d95f4b13b52bee03
AM <73906893+amhappylearning@users.noreply.github.com>
2021-04-29 11:52:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Change image on on Mouse Over. 
+function flipimage() {
+    document.getElementById("image1").src = "https://images.unsplash.com/photo-1531379115628-32ab7454986c?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ";
+  }
+  
